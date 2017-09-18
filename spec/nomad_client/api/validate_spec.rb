373872a2f271fcda3e8b58f3b4c6ac5c995f2667
Jason Gellatly <jason.gellatly@bigcommerce.com>
2017-09-18 11:02:07 +1000

    Add validate spec

---
+++ spec/nomad_client/api/validate_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+module NomadClient
+  module Api
+    RSpec.describe 'Validate' do
+      let!(:nomad_client) { NomadClient::Connection.new('http://nomad.local') }
+
+      describe 'validate' do
+        it 'should add the validate method to the NomadClient::Connection class' do
+          expect(nomad_client).to respond_to :validate
+          expect(nomad_client.validate).to be_kind_of NomadClient::Api::Validate
+        end
+      end
+
+      describe 'Validate API methods' do
+        let(:block_receiver) { double(:block_receiver) }
+        let(:nomad_job)      { { "Job" => {} } }
+        let!(:connection)    { double(:connection) }
+
+        before do
+          allow(nomad_client).to receive(:connection).and_return(connection)
+        end
+
+        describe '#job' do
+          it 'should call post on the validate job endpoint' do
+            expect(connection).to receive(:post).and_yield(block_receiver)
+            expect(block_receiver).to receive(:url).with("validate/job")
+            expect(block_receiver).to receive(:body=).with(nomad_job)
+
+            nomad_client.validate.job(nomad_job)
+          end
+        end
+
+      end
+    end
+  end
+end
